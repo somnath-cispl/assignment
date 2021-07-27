aff4afec302b721c7ce641357fd07943c9bb86a0
Somnath Raj <somnath.raj@codeclouds.in>
2021-07-27 12:15:40 +0530

    update for deploy in heroku

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.listen(3000, () => {
+app.listen( process.env.PORT || 3000, () => {
   console.log("NodeJS Server is running!");
 });
 
